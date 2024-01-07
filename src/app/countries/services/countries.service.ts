d25ae8c6d3598706b90f4466c7fb0224083eae73
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2024-01-07 13:04:40 -0600

    Usign LS

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -26,7 +26,18 @@ export class CountriesService {
     },
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage() {
+    if (!localStorage.getItem('cacheStore')) return;
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  }
 
   private getCountriesRequest(url: string): Observable<Country[]> {
     return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
@@ -38,7 +49,8 @@ export class CountriesService {
       tap(
         (countries: Country[]) =>
           (this.cacheStore.byCapital = { term, countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 
@@ -48,7 +60,8 @@ export class CountriesService {
       tap(
         (countries: Country[]) =>
           (this.cacheStore.byCountry = { term, countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 
@@ -58,7 +71,8 @@ export class CountriesService {
       tap(
         (countries: Country[]) =>
           (this.cacheStore.byRegion = { region, countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 
